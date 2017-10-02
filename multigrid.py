bf631771270bf2091c0ec9777df6c8c6e0321cbe
mheldman <mheldman1@gmail.com>
2017-10-02 08:58:17 -0800

    Update multigrid.py

--- multigrid.py
+++ multigrid.py
@@ -30,8 +30,8 @@ def restrict(U, m, F = zeros(1)):
     n = np.floor(m / 2)
     n = int(n)
     N = (m + 2) ** 2
-    A = np.zeros((n + 2, 3 * (m + 2)))
-    I = np.zeros(((n + 2) ** 2, N))
+    A = zeros((n + 2, 3 * (m + 2)))
+    I = zeros(((n + 2) ** 2, N))
     for i in range(0, n):
         A[i + 1, (2 * i + 1, 2 * i + 2, 2 * i + 3, 2 * i + m + 3,
                   2 * i + m + 4, 2 * i + m + 5, 2 * i + 2 * (m + 2) + 1, 2 * i +
@@ -45,13 +45,11 @@ def restrict(U, m, F = zeros(1)):
 
 def vcycle(m, U, A, F, minm = 25, eta1 = 3, eta2 = 3, numcycles = 5,cyclenum = 0, v = None):
     vh = gs(U, A, F, m, maxiters = eta1)
-    print(vh.shape)
-    print(m,cyclenum)
     if cyclenum < numcycles:
         rh = F - np.dot(A,vh)
         rh = restrict(rh,m)
         m = int(np.floor(m/2))
-        eh = np.zeros(((m+2)**2,1))
+        eh = zeros(((m+2)**2,1))
         cyclenum += 1
         A, U, F, X = obstacle2D(m,f)
         eh = vcycle(m, eh, A, rh, cyclenum = cyclenum, numcycles = numcycles, v = vh)
