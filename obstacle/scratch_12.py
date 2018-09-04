d993740cdce913374e2f7022ed7e9b6bc700263c
mheldman <mheldman1@gmail.com>
2018-09-04 15:47:26 -0400

    updated test file

--- obstacle/scratch_12.py
+++ obstacle/scratch_12.py
@@ -1,4 +1,4 @@
-from diagnostics import elastictorsion_rsp_diagnostic, elastictorsion_pfas_diagnostic, damproblem_PN_diagnostic, radialproblem_pfas_diagnostic, radialproblem_rsp_diagnostic, radialproblem_PN_diagnostic, damproblem_pfas_diagnostic, damproblem_rsp_diagnostic, damproblem_pgs_diagnostic, unconstrproblem_pfas_diagnostic, unconstrproblem_multigrid_diagnostic, radialproblem_pgs_diagnostic
+from obstacle.diagnostics import *
 import cProfile
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,7 +7,7 @@ import numpy as np
 
 #radialproblem_rsp_diagnostic(coarse_mx = 1, coarse_my = 1, min_num_levels = 5, max_num_levels = 6, step = 1)
 
-damproblem_pfas_diagnostic('FW', coarse_mx = 1, coarse_my = 2, min_num_levels = 2, max_num_levels = 15, step = 1, smoothing_iters=2)
+#damproblem_pfas_diagnostic('FW', coarse_mx = 1, coarse_my = 2, min_num_levels = 2, max_num_levels = 15, step = 1, smoothing_iters=2)
 
 #iterates = damproblem_rsp_diagnostic(coarse_mx = 1, coarse_my = 2, min_num_levels = 6, max_num_levels = None, step = 1)
 
